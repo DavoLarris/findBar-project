b394ed978cd44cdb0fa64ac0755b5c9bcb5a9963
DavoLarris <davo.larris@gmail.com>
2017-03-13 16:05:43 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# findBar-project
---
+++ app/src/main/java/org/davolarris/findbar/GoogleApiCliente.java
@@ -0,0 +1,40 @@
+package org.davolarris.findbar;
+
+import android.os.Bundle;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
+
+/**
+ * Created by David on 12/03/2017.
+ */
+
+public class GoogleApiCliente {
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+}
