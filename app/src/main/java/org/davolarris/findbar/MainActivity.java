e9399a0c413e3345c521a76f710a642310511b7d
DavoLarris <davo.larris@gmail.com>
2017-03-16 14:11:10 +0100

    API working and map too

--- app/src/main/java/org/davolarris/findbar/MainActivity.java
+++ app/src/main/java/org/davolarris/findbar/MainActivity.java
@@ -1,13 +1,31 @@
 package org.davolarris.findbar;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button showMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=42.804972, -1.681907&radius=200&types=bar&sensor=false&key=
+
+        showMap = (Button) findViewById(R.id.showMap);
+        showMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                MapFragmentClass mapFragmentClass = new MapFragmentClass();
+                ft.add(R.id.mapLayout, mapFragmentClass);
+                ft.commit();
+            }
+        });
     }
 }
---
+++ app/src/main/java/org/davolarris/findbar/MapFragmentClass.java
@@ -0,0 +1,21 @@
+package org.davolarris.findbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by David on 16/03/2017.
+ */
+
+public class MapFragmentClass extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.map_fragment, container, false);
+        return view;
+    }
+}
