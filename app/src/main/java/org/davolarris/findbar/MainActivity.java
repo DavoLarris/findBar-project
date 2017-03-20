efa4087a1ea2ba3da20d1b52f9b48c99be543549
DavoLarris <davo.larris@gmail.com>
2017-03-20 13:12:50 +0100

    NOw need to access to location

--- app/src/main/java/org/davolarris/findbar/MainActivity.java
+++ app/src/main/java/org/davolarris/findbar/MainActivity.java
@@ -1,31 +1,34 @@
 package org.davolarris.findbar;
 
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+
+public class MainActivity extends AppCompatActivity{
     private Button showMap;
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=42.804972, -1.681907&radius=200&types=bar&sensor=false&key=
 
+
         showMap = (Button) findViewById(R.id.showMap);
         showMap.setOnClickListener(new View.OnClickListener() {
-            @Override
+           @Override
             public void onClick(View view) {
                 FragmentManager fm = getFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                MapFragmentClass mapFragmentClass = new MapFragmentClass();
-                ft.add(R.id.mapLayout, mapFragmentClass);
-                ft.commit();
+                fm.beginTransaction().add(R.id.mapLayout, new MapFragmentClass()).commit();
             }
         });
     }
+
+
 }
--- app/src/main/java/org/davolarris/findbar/MapFragmentClass.java
+++ app/src/main/java/org/davolarris/findbar/MapFragmentClass.java
@@ -1,21 +1,54 @@
 package org.davolarris.findbar;
 
+
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Created by David on 16/03/2017.
  */
 
-public class MapFragmentClass extends Fragment {
-    @Nullable
-    @Override
+public class MapFragmentClass extends Fragment implements OnMapReadyCallback {
+    private GoogleMap googleMap;
+    private MapFragment mapFragment;
+    private View mView;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.map_fragment, container, false);
-        return view;
+        mView = inflater.inflate(R.layout.map_fragment, container, false);
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fragmentMap);
+        return mView;
     }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (mapFragment == null) { //Need to take a look at this... This is a chapuza
+            mapFragment = MapFragment.newInstance();
+
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragmentMap, mapFragment);
+            fragmentTransaction.commit();
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(42.804972, -1.681907), 16));
+
+    }
+
+
 }
